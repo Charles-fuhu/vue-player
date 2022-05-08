4f3abda39f48a24dd6ff8364dd70e562347b64a4
侯明君 <houmingjun2025@163.com>
2022-05-08 23:00:32 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ export default {
     key,
   },
   created() {
-    console.log('welcome,这是我的第一个前端项目，右上角有源码地址，如果有帮助的话可以给一颗star,接口地址http://124.223.37.254:3000/')
+    console.log('welcome!,这是我的第一个前端项目，右上角有源码地址，如果有帮助的话可以给一颗star,接口地址http://124.223.37.254:3000/')
   }
 };
 </script>
